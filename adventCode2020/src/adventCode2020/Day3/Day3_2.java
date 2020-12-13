0fe0048b7473e82473c54043df29717e44dfead7
Samuel <Samuel@DESKTOP-1LP1428.technicolor.net>
2020-12-13 03:58:25 +0100

    Needs to be checked

--- adventCode2020/src/adventCode2020/Day3/Day3_2.java
+++ adventCode2020/src/adventCode2020/Day3/Day3_2.java
@@ -7,9 +7,20 @@ import java.util.Scanner;
 
 public class Day3_2 {
 	public static void main (String args[]) throws FileNotFoundException {
-		countTrees(1,3);
+		int tree1 = countTrees(1,1);
+		System.out.println(tree1);
+		int tree2 = countTrees(1,3);
+		System.out.println(tree2);
+		int tree3 = countTrees(1,5);
+		System.out.println(tree3);
+		int tree4 = countTrees(1,7);
+		System.out.println(tree4);
+		int tree5 = countTrees(2,1);
+		System.out.println(tree5);
+		int finalResult = (tree1*tree2*tree3*tree4*tree5);
+		System.out.println(finalResult);
 	}
-	static void countTrees(int down, int right) throws FileNotFoundException {
+	static int countTrees(int down, int right) throws FileNotFoundException {
 		int count = 0;
 		char a = '#';
 		int j = 0;
@@ -19,12 +30,12 @@ public class Day3_2 {
 			map.add(fileScanner.nextLine());
 		}
 		int lineSize = (map.get(0).length());
-		for (int i=1; i<map.size(); i++) {
+		for (int i=1; i<map.size(); i = i + down) {
 			j = j+right;
-			if(map.get(i).charAt(j%lineSize) == a) {
+			if(map.get(i%map.size()).charAt(j%lineSize) == a) {
 				count++;
 			}
 		}
-		System.out.println(count);
+		return count;
 	}
 }
