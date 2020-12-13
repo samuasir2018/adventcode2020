d71489f87173a1ab8d8610f3e92ae8aa06c799ce
Samuel <Samuel@DESKTOP-1LP1428.technicolor.net>
2020-12-13 03:24:04 +0100

    Day3_1 completed

---
+++ adventCode2020/src/adventCode2020/Day3/Day3_1.java
@@ -0,0 +1,27 @@
+package adventCode2020.Day3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day3_1 {
+	public static void main(String args[]) throws FileNotFoundException {
+		int count = 0;
+		char a = '#';
+		int j = 0;
+		ArrayList<String> map = new ArrayList<String>();
+		Scanner fileScanner = new Scanner(new File("src/adventCode2020/Day3/input.txt"));
+		while (fileScanner.hasNext()) {
+			map.add(fileScanner.nextLine());
+		}
+		int lineSize = (map.get(0).length());
+		for (int i=1; i<map.size(); i++) {
+			j = j+3;
+			if(map.get(i).charAt(j%lineSize) == a) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
